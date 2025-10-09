ef9595bb7ea3e9edfc85615888a4bcfbcac52328
Benson <benson@wisefido.com>
2025-10-09 13:55:28 +0800

    让 Vault CLI 不去验证 TLS

--- 04_scripts/02_setup_unseal_vault.sh
+++ 04_scripts/02_setup_unseal_vault.sh
@@ -4,8 +4,9 @@ set -euo pipefail
 read -p "输入第一个 Unseal Key: " key1
 read -p "输入第二个 Unseal Key: " key2
 
-docker exec -i wisefido-vault vault operator unseal "$key1"
-docker exec -i wisefido-vault vault operator unseal "$key2"
+docker exec -e VAULT_SKIP_VERIFY=true -i wisefido-vault vault operator unseal "$KEY1"
+docker exec -e VAULT_SKIP_VERIFY=true -i wisefido-vault vault operator unseal "$KEY2"
+
 
 docker exec -i wisefido-vault vault status
 echo "✅ Vault 已成功解封。"
