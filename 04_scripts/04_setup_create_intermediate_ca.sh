2a937b11f84c1217ac31ff360615d2732d063fae
Benson <benson@wisefido.com>
2025-10-09 14:26:53 +0800

    04_setup_create_intermediate_ca.sh

--- 04_scripts/04_setup_create_intermediate_ca.sh
+++ 04_scripts/04_setup_create_intermediate_ca.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 PROJECT_ROOT="/opt/00_WiseFido_CA_Project"
-INT_DIR="${PROJECT_ROOT}/05_opt/01_wisefido-ca/02_intermediate"
 
 read -p "请输入 Vault Root Token: " token
-export VAULT_ADDR="https://ca.wisefido.work:8200"
+export VAULT_ADDR="https://127.0.0.1:8200"
 export VAULT_TOKEN="$token"
-export VAULT_SKIP_VERIFY=true
+export VAULT_SKIP_VERIFY=true   # ← 添加这行，跳过 TLS 校验
 
-# 启用 Intermediate PKI 引擎
-docker exec -i wisefido-vault vault secrets enable -path=pki_int pki
-docker exec -i wisefido-vault vault secrets tune -max-lease-ttl=43800h pki_int
+# 启用 Root PKI
+docker exec -e VAULT_SKIP_VERIFY=true -i wisefido-vault vault secrets enable pki
+docker exec -e VAULT_SKIP_VERIFY=true -i wisefido-vault vault secrets tune -max-lease-ttl=87600h pki
 
 # 生成 Intermediate CSR
 docker exec -i wisefido-vault vault write -field=csr pki_int/intermediate/generate/internal \
